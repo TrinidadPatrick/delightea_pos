d4fd64add36de130a7d07acf0652b2683b441d41
John Patrick Trinidad <Ptrinidad765@gmail.com>
2025-03-23 08:45:20 +0800

    updated

--- http.js
+++ http.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://192.168.100.34:5000/api/',
+  // baseURL: 'http://192.168.100.34:5000/api/',
   // baseURL: 'http://192.168.0.17:5000/api/',
   // // baseURL: 'http://localhost:5000/api/',
-  // baseURL: 'https://delightea-be.vercel.app/api/',
+  baseURL: 'https://delightea-be.vercel.app/api/',
 })
 
 export default http;
